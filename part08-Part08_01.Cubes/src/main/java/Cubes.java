e955a86eb9566752dc085cbae935cf904c4e89de
Ali Ahmed <alitine94@gmail.com>
2024-07-31 03:15:37 -0400

    exercises

--- part08-Part08_01.Cubes/src/main/java/Cubes.java
+++ part08-Part08_01.Cubes/src/main/java/Cubes.java
@@ -5,6 +5,21 @@ public class Cubes {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        while(true) {
+            System.out.println("Number?");
+            String input = scanner.nextLine();
+
+            if(input.equals("end")) {
+                break;
+            }
+
+            int number = Integer.valueOf(input);
+            int cubed = number * number * number;
+            
+           
+            System.out.println(cubed);
+        }
 
     }
 }
