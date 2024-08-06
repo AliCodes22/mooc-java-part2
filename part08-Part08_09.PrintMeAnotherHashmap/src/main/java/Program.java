04ed0377c875d19d601596112eb5215bc4851847
Ali Ahmed <alitine94@gmail.com>
2024-08-06 03:09:09 -0400

    exercises

--- part08-Part08_09.PrintMeAnotherHashmap/src/main/java/Program.java
+++ part08-Part08_09.PrintMeAnotherHashmap/src/main/java/Program.java
@@ -7,4 +7,19 @@ public class Program {
         // Test your program here!
     }
 
+    public static void printValues(HashMap<String,Book> hashmap) {
+        for(Book book: hashmap.values()){
+            System.out.println("Name: " + book.getName() + "(" + book.getPublicationYear() + ")");
+            System.out.println("Contents: " + book.getContents());
+        }
+    }
+
+    public static void printValueIfNameContains(HashMap<String,Book> hashmap, String text) {
+        for(Book book:hashmap.values()) {
+            if(book.getName().contains(text)) {
+                System.out.println(book.toString());
+            }
+        }
+    }
+
 }
