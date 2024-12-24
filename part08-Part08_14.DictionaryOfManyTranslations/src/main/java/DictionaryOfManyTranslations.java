8c27110db14771a2779393b0ce8ae7f1664f26cc
Ali Ahmed <alitine94@gmail.com>
2024-12-24 05:43:32 -0500

    part 4 done

--- part08-Part08_14.DictionaryOfManyTranslations/src/main/java/DictionaryOfManyTranslations.java
+++ part08-Part08_14.DictionaryOfManyTranslations/src/main/java/DictionaryOfManyTranslations.java
@@ -1,34 +1,38 @@
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.ArrayList;
 
-public class DictionaryOfManyTranslations{
-    private HashMap<String,ArrayList<String>> translations;
+public class DictionaryOfManyTranslations {
+    private HashMap<String, ArrayList<String>> translations;
 
     public DictionaryOfManyTranslations() {
         this.translations = new HashMap<>();
     }
 
-    public void add(String word,String translation) {
-        if(!this.translations.containsKey(word)){
-            this.translations.put(word, new ArrayList<>());
-        }
+    public void add(String word, String translation) {
+        // create new entry if it doesn't exist
+       this.translations.putIfAbsent(word, new ArrayList<String>());
 
-        this.translations.get(word).add(translation);
+       // now add the translation to the provided word
+       this.translations.get(word).add(translation);
     }
 
     public ArrayList<String> translate(String word) {
-        if(this.translations.get(word) == null) {
+
+        // get values associated to the word
+        ArrayList<String> values = this.translations.get(word);
+
+        // before returning it, check whether they're null or not, or
+        // if the word has no translations
+        if(values == null) {
             return new ArrayList<>();
         }
 
-        return this.translations.get(word);
+        return values;
     }
 
     public void remove(String word) {
-        if(this.translations.get(word) == null) {
-        return;
-       }
         this.translations.remove(word);
     }
 
+    
 }
--- part08-Part08_15.StorageFacility/src/main/java/StorageFacility.java
+++ part08-Part08_15.StorageFacility/src/main/java/StorageFacility.java
@@ -1,29 +1,58 @@
 import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
 import java.util.ArrayList;
 
-public class StorageFacility{
-    private HashMap<String, ArrayList<String>> storage;
+public class StorageFacility {
+    private HashMap<String,ArrayList<String>> storage;
 
     public StorageFacility() {
         this.storage = new HashMap<>();
     }
 
     public void add(String unit, String item) {
-        if(!this.storage.containsKey(unit)) {
-            this.storage.put(unit, new ArrayList<>());
-        }
+        this.storage.putIfAbsent(unit, new ArrayList<String>());
 
         this.storage.get(unit).add(item);
     }
 
     public ArrayList<String> contents(String storageUnit) {
-        if(!this.storage.containsKey(storageUnit)) {
-            this.storage.put(storageUnit, new ArrayList<>());
-        } 
+        // get the list items using the key value
+        ArrayList<String> items = this.storage.get(storageUnit);
 
-        return this.storage.get(storageUnit);
+        // if the HashMap doesn't contain the provided key
+        // or if the list is empty, return an empty list
+        if(items == null) {
+            return new ArrayList<>();
+        }
 
-        
+        // if all is good return items
+        return items;
     }
 
+    public void remove(String storageUnit, String item) {
+       ArrayList<String> items = this.storage.get(storageUnit);
+
+        if(items != null) {
+            items.remove(item);
+
+            if(items.isEmpty()) {
+                this.storage.remove(storageUnit);
+            }
+        }
+    }
+
+    public ArrayList<String> storageUnits() {
+        ArrayList<String> units = new ArrayList<>();
+
+        for(String key: this.storage.keySet()) {
+            ArrayList<String> items = this.storage.get(key);
+
+            if(!items.isEmpty()) {
+                units.add(key);
+            }
+        }
+
+        return units;
+    }
 }
