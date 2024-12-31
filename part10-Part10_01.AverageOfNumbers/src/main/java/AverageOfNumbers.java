8846b7ca4d01701dfbae55f73ebf584930bd6c6d
Ali Ahmed <alitine94@gmail.com>
2024-12-31 06:32:32 -0500

    streams

--- part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -1,12 +1,30 @@
 
 import java.util.ArrayList;
+import java.util.OptionalDouble;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class AverageOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // Write your program here
+        
+        ArrayList<Integer> nums = new ArrayList<>();
 
+        while(true) {
+            String value = scanner.nextLine();
+
+            if(value.equals("end")) {
+                break;
+            }
+            int numValue = Integer.valueOf(value);
+            nums.add(numValue);
+
+        }
+
+        OptionalDouble average = nums.stream().mapToInt(Integer::intValue).average();
+
+        System.out.println("average of the numbers: " + average);
     }
 }
