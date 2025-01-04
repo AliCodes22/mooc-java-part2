dadf37a7102fbcce323a2ea96f3eb56b15cfd0db
Ali Ahmed <alitine94@gmail.com>
2025-01-04 07:49:01 -0500

    comparator

---
+++ part10-Part10_14.Literature/src/main/java/Book.java
@@ -0,0 +1,25 @@
+import java.util.Comparator;
+
+public class Book {
+    private String name;
+    private int ageRecommendation;
+
+    public Book(String name, int ageRecommendation) {
+        this.name = name;
+        this.ageRecommendation = ageRecommendation;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAgeRecommendation() {
+        return this.ageRecommendation;
+    }
+
+    @Override
+    public String toString() {
+        return this.getName() + " (recommended for " + this.getAgeRecommendation() + " year-olds or older)"; 
+    }
+    
+}
--- part10-Part10_14.Literature/src/main/java/MainProgram.java
+++ part10-Part10_14.Literature/src/main/java/MainProgram.java
@@ -1,10 +1,43 @@
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Comparator;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.Collections;
 
 public class MainProgram {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        List<Book> books = new ArrayList<>();
+
+        while(true) {
+            System.out.println("Input the name of the book, empty stops:");
+            String input = scanner.nextLine();
+
+            if(input.isEmpty()) {
+                break;
+            }
+
+            System.out.println("Input the age recommendation:");
+            int age = Integer.valueOf(scanner.nextLine());
+
+            Book book = new Book(input, age);
+            books.add(book);
+        }
+
+        System.out.println(books.size() + " books in total.");
+
+        System.out.println("Books:");
+       
+        Comparator<Book> comparator = Comparator.comparing(Book::getAgeRecommendation).thenComparing(Book::getName);
+
+        Collections.sort(books,comparator);
+
+        for(Book book: books) {
+            System.out.println(book);
+        }
 
     }
 
