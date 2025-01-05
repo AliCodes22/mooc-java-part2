c04fdef806442f428ab98f3e9eced85cbb89ed53
Ali Ahmed <alitine94@gmail.com>
2025-01-05 06:38:57 -0500

    classes

--- part11-Part11_02.ABookAndAPlane/src/main/java/Program.java
+++ part11-Part11_02.ABookAndAPlane/src/main/java/Program.java
@@ -6,6 +6,8 @@ public class Program {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // you can test your classes here
+        Plane plane = new Plane(null, null, 0);
+        Book book = new Book(null, null, 0);
 
     }
 }
