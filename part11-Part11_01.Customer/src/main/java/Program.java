e1d3740f194f5758df06666f06e6be29d9f4c46c
Ali Ahmed <alitine94@gmail.com>
2025-01-05 05:41:50 -0500

    part 11

--- part11-Part11_01.Customer/src/main/java/Program.java
+++ part11-Part11_01.Customer/src/main/java/Program.java
@@ -6,6 +6,9 @@ public class Program {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // you can test your class here
+        Customer customer = new Customer(
+          "Ali"  
+        , "8325 blvd de l'Acadie", "alitine94@gmail.com");
 
     }
 }
