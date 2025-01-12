f9c0c1cd6552da0a940b8838fb363d88e8072d56
Ali Ahmed <alitine94@gmail.com>
2025-01-12 06:56:41 -0500

    random

--- part12-Part12_03.SumTheseForMe/src/main/java/Program.java
+++ part12-Part12_03.SumTheseForMe/src/main/java/Program.java
@@ -1,4 +1,5 @@
 
+
 import java.util.Scanner;
 
 public class Program {
@@ -7,6 +8,24 @@ public class Program {
         Scanner scan = new Scanner(System.in);
         // test your method here
 
+        
+
+    }
+
+    public static int sum(int[] array, int fromWhere, int toWhere, int smallest, int largest) {
+        int sum = 0;
+
+        if(fromWhere < 0 || toWhere >= array.length) {
+            fromWhere = 0;
+            toWhere = array.length - 1;
+        }
+        for(int i = fromWhere; i <= toWhere; i++) {
+            if(array[i] >= smallest && array[i] <= largest) {
+                sum += array[i];
+            }
+        }
+
+        return sum;
     }
 
 }
--- part12-Part12_06.Numbers/src/main/java/Program.java
+++ part12-Part12_06.Numbers/src/main/java/Program.java
@@ -6,6 +6,15 @@ public class Program {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Random generateRandom = new Random();
+
+        System.out.println("How many numbers should be printed?");
+        int value = Integer.valueOf(scanner.nextLine());
+
+        for(int i  = 0; i < value; i++) {
+            int randomNumber = generateRandom.nextInt(11);
+            System.out.println(randomNumber);
+        }
     }
 
 }
